6c192604b1efda3d9a3a9fcf891985e4b0866f80
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-17 10:33:25 -0500

    Zoho gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
+require 'ruby_zoho'
+
 class HomeController < ApplicationController
 
   def index
+    $leads = RubyZoho::Crm::Account.all
   end
-  
+
 end
--- config/initializers/zoho.rb
+++ config/initializers/zoho.rb
@@ -1,10 +1,11 @@
-#require 'ruby_zoho'
+require 'ruby_zoho'
 
-#RubyZoho.configure do |config|
-#  config.api_key = ENV["zoho_api_key"]
+RubyZoho.configure do |config|
+  config.api_key = ENV["zoho_api_key"]
+  config.crm_modules = ['Leads']
   # config.crm_modules = ['Accounts', 'Contacts', 'Leads', 'Potentials'] # Defaults to free edition if not set
   # config.crm_modules = ['Quotes', 'SalesOrders', 'Invoices'] # Depending on what kind of account you've got, adds to free edition modules
   # config.ignore_fields_with_bad_names = true # Ignores field with special characters in the name (Release 1.8)
   # Currently only Quotes are supported
-#  config.cache_fields = true
-#end
+  config.cache_fields = true
+end
